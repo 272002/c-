585bcca7b9b3b8652cf00588e65083b811b1f8a4
272002 <108273339+amanjain272002@users.noreply.github.com>
2023-06-15 11:37:00 +0530

    commit

---
+++ iterInorder.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+class Node
+{
+    public:
+    int data;
+    Node *left, *right;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->left = this->right = nullptr;
+    }
+};
+
+void iterInorder(Node* root){
+    stack<Node*> st;
+    Node* curr = root;
+    while (!st.empty() || curr != nullptr)
+    {
+        if (curr == NULL)
+        {
+            curr = st.top();
+            cout<<curr->data<<" ";
+            st.pop();
+            curr = curr->right;
+        }
+        else
+        {
+            st.push(curr);
+            curr = curr->left;
+        }
+        
+    }
+    
+}
+
+
+int main(){
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->right->left = new Node(5);
+    root->right->right = new Node(6);
+    root->right->left->left = new Node(7);
+    root->right->left->right = new Node(8);
+
+    iterInorder(root);
+    return 0;
+}
